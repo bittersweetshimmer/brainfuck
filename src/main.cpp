2a27536f09df1225151dd14ac2d77dc96be33b3f
bittersweetshimmer <bttrswt@protonmail.com>
2020-04-13 09:51:07 +0200

    Remove debugging stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,5 @@
 
 int main(int argc, char* argv[]) {
   if (argc <= 1) { std::cout << bf::util::console::bold_white << "brainfuck" << bf::util::console::reset << ": no input" << std::endl; return EXIT_SUCCESS; }
-  for (int i = 0; i < argc; ++i) {
-    std::cout << argv[i] << std::endl;
-  }
-  return bf::State(bf::parse(argv[1])).run();
+  else return bf::State(bf::parse(argv[1])).run();
 }
